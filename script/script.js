b32faf09da7c167f3872e43a12ca81849ea6f13d
Stefan Hiob <stefan.hiob@outlook.de>
2023-07-31 18:53:17 +0200

    clean code

--- script/script.js
+++ script/script.js
@@ -20,3 +20,22 @@ const sectionOneObserver = new IntersectionObserver(function (
 sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+const toggle = document.getElementById("switch-toggle");
+
+toggle.addEventListener("change", function () {
+  if (this.checked) {
+    switchTheme();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    switchTheme();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+let switchTheme = () => {
+  document.documentElement.classList.add("theme-transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("theme-transition");
+  }, 1000);
+};
